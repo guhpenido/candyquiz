702a78629d70fccfe33d8869c98401873ba6a539
Gustavo Marcelo Penido Pereira <82380592+guhpenido@users.noreply.github.com>
2022-01-05 09:05:00 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,70 @@
+function abrirModalUm() {
+    let modalUm = document.querySelector(".modal-um");
+
+    modalUm.style.display = "block";
+}
+
+function fecharModalUm() {
+    let modalUm = document.querySelector(".modal-um");
+
+    modalUm.style.display = "none";
+    let inputNomeEl = document.querySelector("#nome-input");
+    let spanNomeDesktopEl = document.querySelector("#nome-destkop");
+    let spanNomeMobileEl = document.querySelector("#nome-mobile");
+
+    spanNomeDesktopEl.innerHTML = inputNomeEl.value;
+    spanNomeMobileEl.innerHTML = inputNomeEl.value;
+}
+
+function abrirModalDois() {
+    let modalDois = document.querySelector(".modal-dois");
+
+    modalDois.style.display = "block";
+    fecharModalUm()
+}
+
+function fecharModalDois() {
+    let modalDois = document.querySelector(".modal-dois");
+
+    modalDois.style.display = "none";
+}
+
+
+function irParaMenu() {
+    window.location.href = "/menu.html";
+}
+
+/*------------MENU----------------*/
+
+let video = document.getElementById("videoBackground");
+let som = document.querySelector("#som");
+let links = document.querySelectorAll("ul a");
+let audioClick = document.getElementById("audioClick");
+let audioHover = document.getElementById("audioHover");
+
+
+som.addEventListener("click", () => {
+    som.classList.toggle("fa-volume-up");
+
+    if (video.muted === false) {
+        video.muted = true;
+    } else {
+        video.muted = false;
+    }
+    clickSound();
+});
+
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", clickSound);
+    links[i].addEventListener("mouseenter", hoverSound);
+}
+
+function clickSound() {
+    audioClick.play();
+}
+
+function hoverSound() {
+    audioHover.play();
+}
+
+/*-------------------------------------*/
